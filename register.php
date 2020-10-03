446ff812a2eaa7845653fbb4706df293639c6703
Bryan <bryan_desarrollo_ec@outlook.com>
2020-10-02 23:52:43 -0500

    realizando las validaciones del formulario de registro

--- register.php
+++ register.php
@@ -1,15 +1,63 @@
 <?php
 
-$name;
-$lastName;
-$email;
-$password;
+$name = '';
+$lastName = '';
+$email = '';
+$password = '';
+$errors = array();
 
-var_dump($_POST);
 
+// verificar si todos los pararemtros estan siendo enviados por la url
 if(!isset($_POST['nombre']) || !isset($_POST['apellidos']) || !isset($_POST['email']) || !isset($_POST['password'])) {
     echo "faltan parametros";
 }
 
+// verificar que los parametros no se encuentren vacios y cumplan las reglas de validación
 
-?>
+$name = empty($_POST['nombre']) ? error('El campo nombre no puede estar en blanco') : $_POST['nombre'];
+$lastName = empty($_POST['apellidos']) ? error('El campo apellidos no puede estar en blanco') : $_POST['apellidos'];
+$email = empty($_POST['email']) ? error('El campo correo electronico no puede estar en blanco') : $_POST['email'];
+$password = empty($_POST['password']) ? error('El campo password no puede estar en blanco') : $_POST['password'];
+
+validateErrors();
+
+// validar la información de los campos ingresados.
+if(preg_match('/[0-9]/',$name))
+    error('El campo nombre solo puede contenecer letras');
+
+if(preg_match('/[0-9]/',$lastName))
+    error('El campo apellidos solo puede contenecer letras');
+
+if(!filter_var($email,FILTER_VALIDATE_EMAIL))
+    error('La dirección de correo electronico no es valida');
+
+validateErrors();
+
+
+
+
+
+
+
+
+// -------------------- FUNCIONES ---------------------------
+
+
+// agregar los errores al array errors
+function error($error){
+    global $errors;
+    array_push($errors,$error);
+}
+
+// en caso de existir errores [$errors] mostrara por pantalla la lista de los errores presentados
+function validateErrors() {
+    global $errors;
+    if(!empty($errors)){
+        // mostrar mensajes de error por pantalla
+        var_dump($errors);
+    }
+}
+
+
+
+
